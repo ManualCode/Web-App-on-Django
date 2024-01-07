fee5171de931afb8e2ca9eae506bd9d74ba7af84
ManualCode <kiriltimoxov@gmail.com>
2024-01-07 15:10:24 +0500

    second commit

--- djangoProject/main/views.py
+++ djangoProject/main/views.py
@@ -6,12 +6,12 @@ def index(request):
 
 
 def relevance(request):
-    return render(request, "main/about.html")
+    return render(request, "main/relevance.html")
 
 
 def geography(request):
-    return render(request, "main/about.html")
+    return render(request, "main/geography.html")
 
 
 def recent_vacancies(request):
-    return render(request, "main/about.html")
+    return render(request, "main/recent_vacancies.html")
