0355e36582cf24decb5d19e56ac0e92fb5115759
ManualCode <kiriltimoxov@gmail.com>
2024-01-15 13:47:11 +0500

    4 commit

--- Analytics Scripts/analitics.py
+++ Analytics Scripts/analitics.py
@@ -1,68 +1,16 @@
-from concurrent.futures import ThreadPoolExecutor
-import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-import requests
-import math
-import time
 
 
-def calculate_salary(row):
-    salary_from = row['salary_from']
-    salary_to = row['salary_to']
-    currency = row['salary_currency']
-    published_at = row['published_at']
-
-    if pd.isna(salary_from) and pd.isna(salary_to):
-        return 0
-    if pd.isna(salary_from):
-        salary = math.floor(salary_to)
-    elif pd.isna(salary_to):
-        salary = math.floor(salary_from)
-    else:
-        salary = (salary_from + salary_to) / 2
-    if currency == 'RUR':
-        return salary
-
-    date = pd.to_datetime(published_at, format="%Y-%m-%d")
-    response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp', params={'date_req': date.strftime('%d/%m/%Y')})
-    if response.status_code == 200:
-        # df = pd.read_xml(StringIO(response.text), xpath='.//Valute')
-        # exchange_rate = float(df[df['CharCode'] == currency]['VunitRate'].values[0].replace(',', '.'))
-        root = ET.fromstring(response.text)
-        valute = root.find(f".//Valute[CharCode='{currency}']")
-        exchange_rate = float(valute.find("VunitRate").text.replace(',', '.'))
-        #print((salary, currency, published_at, '-->', exchange_rate))
-        if pd.isna(exchange_rate):
-            return 0
-
-        return salary * exchange_rate
-    else:
-        return 0
-
-
-def calculate_salary_parallel(data):
-    with ThreadPoolExecutor() as executor:
-        results = executor.map(calculate_salary, data)
-    return list(results)
-
-
-def create_cur_data(csv='C:\\Users\\kiril\\PycharmProjects\\Web-App-on-Django\\files\\vacancies.csv'):
-    data = pd.read_csv(csv, nrows=10000)
-    start = time.time()
-    #data['salary'] = data.apply(lambda row: calculate_salary(row['salary_from'], row['salary_to'], row['salary_currency'], row['published_at']), axis=1)
-    data['salary'] = calculate_salary_parallel(data.to_dict('records'))
-    data['published_at'] = data['published_at'].apply(lambda x: x[:10:])
-    data['published_at'] = pd.to_datetime(data['published_at'], format="%Y-%m-%d")
-    data['year'] = data['published_at'].dropna().dt.year
-    data.to_csv('C:\\Users\\kiril\\PycharmProjects\\Web-App-on-Django\\files\\vac.csv')
-    end = time.time()
-    print((end-start) * 10**3)
+def create_cur_data(csv='C:\\Users\\kiril\\PycharmProjects\\Web-App-on-Django\\files\\cur_vac.csv'):
+    data = pd.read_csv(csv)
+    data = data.loc[data['salary'] > 0]
     return data
 
 
 def filter_data(data):
-    data = data[data['name'].str.lower().str.contains("|".join(['с#']), case=False, na=False)]
+    data = data[data['name'].str.lower().str.contains("|".join(['c#', 'c sharp', 'шарп', 'с#']), case=False, na=False)]
     return data
 
 
@@ -70,34 +18,34 @@ def get_salary_dynamics(data):
     salary_dynamics = data.fillna(0).groupby('year')['salary'].mean().astype(float).round().to_dict()
 
     if len(salary_dynamics) != 21:
-        dict = {}
+        dict_ = {}
         for i in range(2003, 2024):
             if i in salary_dynamics:
-                dict[i] = salary_dynamics[i]
+                dict_[str(i)] = salary_dynamics[i]
             else:
-                dict[i] = 0
+                dict_[str(i)] = 0
 
-        salary_dynamics = dict
-
+        salary_dynamics = dict_
     return salary_dynamics
 
 
 def get_vacancies_dynamics(data):
     vacancy_dynamics = data.groupby('year')['name'].count().to_dict()
     if len(vacancy_dynamics) != 21:
-        dict = {}
+        dict_ = {}
         for i in range(2003, 2024):
             if i in vacancy_dynamics:
-                dict[i] = vacancy_dynamics[i]
+                dict_[str(i)] = vacancy_dynamics[i]
             else:
-                dict[i] = 0
+                dict_[str(i)] = 0
 
-        vacancy_dynamics = dict
+        vacancy_dynamics = dict_
     return vacancy_dynamics
 
 
 def get_top_cities_salary(selected_data):
-    df_area_salaries = selected_data.groupby(['area_name']).agg({'salary': 'mean', 'name': 'count'}).dropna().astype(float).round().astype(int).sort_values(by=['salary', 'area_name'], ascending=(False, True))
+    df_area_salaries = selected_data.groupby(['area_name']).agg({'salary': 'mean', 'name': 'count'}).dropna()\
+        .astype(float).round().astype(int).sort_values(by=['salary', 'area_name'], ascending=(False, True))
     df_area_salaries['name'] = df_area_salaries['name'] / df_area_salaries['name'].sum()
     df_area_salaries = df_area_salaries[df_area_salaries.name * 100 >= 1]
     city_salary = {key: value for key, value in list(df_area_salaries.to_dict()['salary'].items())[:10]}
@@ -105,7 +53,8 @@ def get_top_cities_salary(selected_data):
 
 
 def get_top_cities_vacancies(selected_data):
-    df_area_vacancies = selected_data.groupby(['area_name']).agg({'name': 'count'}).sort_values(by=['name', 'area_name'], ascending=(False, True))
+    df_area_vacancies = selected_data.groupby(['area_name']).agg({'name': 'count'})\
+        .sort_values(by=['name', 'area_name'], ascending=(False, True))
     df_area_vacancies['name'] = df_area_vacancies['name'] / df_area_vacancies['name'].sum()
     df_area_vacancies = df_area_vacancies[df_area_vacancies.name * 100 >= 1]
     city_vacancy_ratio = {key: round(value, 4) for key, value in list(df_area_vacancies.to_dict()['name'].items())[:10]}
@@ -113,61 +62,76 @@ def get_top_cities_vacancies(selected_data):
     return city_vacancy_ratio
 
 
-def create_plot():
-    data = create_cur_data()
-    filtred_data = filter_data(data)
-    fig, sub = plt.subplots(2, 2)
-
-    #   1 ГРАФИК
+def first_plot(data, filtred_data):
     salary_dynamics = get_salary_dynamics(data)
     filtred_salary_dynamics = get_salary_dynamics(filtred_data)
-    sub[0, 0].set_title(label="Уровень зарплат по годам", fontsize=8)
-    sub[0, 0].bar(salary_dynamics.keys(), salary_dynamics.values())
-    sub[0, 0].bar(salary_dynamics.keys(), filtred_salary_dynamics.values())
-    sub[0, 0].legend(['средняя з/п', 'з/п C#'], fontsize=8)
-    sub[0, 0].grid(axis='y')
-    sub[0, 0].set_xticks([x for x in range(2003, 2024)], labels=[x for x in range(2003, 2024)],
-                         rotation=90, fontsize=8)
-    sub[0, 0].tick_params(axis='y', labelsize=8)
+    plt.title(label="Уровень зарплат по годам", fontsize=8)
+    x_axis = np.arange(len(list(salary_dynamics.keys())))
+    plt.bar(x_axis - 0.2, salary_dynamics.values(), width=0.4)
+    plt.bar(x_axis + 0.2, filtred_salary_dynamics.values(), width=0.4)
+    plt.legend(['Cредняя З/П', 'З/П C#-программиста'], fontsize=8)
+    plt.grid(axis='y')
+    plt.xticks(x_axis, labels=[x for x in range(2003, 2024)], rotation=90, fontsize=8)
+    plt.tick_params(axis='y', labelsize=8)
+    plt.show()
 
-    #           2 ГРАФИК
+
+def second_plot(data, filtred_data):
     vacancies_dynamics = get_vacancies_dynamics(data)
     filtred_vacancies_dynamics = get_vacancies_dynamics(filtred_data)
-    sub[0, 1].set_title(label="Количество вакансий по годам", fontsize=8)
-    sub[0, 1].bar(vacancies_dynamics.keys(), vacancies_dynamics.values())
-    sub[0, 1].bar(vacancies_dynamics.keys(), filtred_vacancies_dynamics.values())
-    sub[0, 1].legend(['Количество вакансий', f'Количество вакансий C#'], fontsize=8)
-    sub[0, 1].set_xticks([x for x in range(2003, 2024)], labels=[x for x in range(2003, 2024)],
-                         rotation=90, fontsize=8)
-    sub[0, 1].tick_params(axis='y', labelsize=8)
+    x_axis = np.arange(len(list(vacancies_dynamics.keys())))
+    plt.title(label="Количество вакансий по годам", fontsize=8)
+    plt.bar(x_axis - 0.2, vacancies_dynamics.values(), width=0.4)
+    plt.bar(x_axis + 0.2, filtred_vacancies_dynamics.values(), width=0.4)
+    plt.ylim(0, 20_000)
+    plt.legend(['Количество вакансий', 'Количество вакансий C#-программист'], fontsize=8)
+    plt.xticks(x_axis, labels=[x for x in range(2003, 2024)], rotation=90, fontsize=8)
+    plt.tick_params(axis='y', labelsize=8)
+    plt.show()
 
-    #           3 ГРАФИК
+
+def third_plot(data):
     top_cities_salary = get_top_cities_salary(data)
     f = []
     for i in list(top_cities_salary.keys()):
-        if ' ' in i: f.append('\n'.join(i.split(" ")))
-        elif '-' in i: f.append('-\n'.join(i.split("-")))
-        else: f.append(i)
+        if ' ' in i:
+            f.append('\n'.join(i.split(" ")))
+        elif '-' in i:
+            f.append('-\n'.join(i.split("-")))
+        else:
+            f.append(i)
 
-    sub[1, 0].set_title(label="Уровень зарплат по городам", fontsize=8)
-    sub[1, 0].barh(list(top_cities_salary.keys()), top_cities_salary.values())
-    sub[1, 0].set_yticklabels(f, fontdict={'horizontalalignment': 'right',
-                                                           'verticalalignment': 'center'})
-    sub[1, 0].tick_params(axis='y', labelsize=6)
-    sub[1, 0].tick_params(axis='x', labelsize=8)
-    sub[1, 0].grid(axis='x')
-    sub[1, 0].invert_yaxis()
+    plt.title(label="Уровень зарплат по городам", fontsize=8)
+    plt.barh(list(top_cities_salary.keys()), top_cities_salary.values())
+    plt.yticks(list(top_cities_salary.keys()), f, fontdict={'horizontalalignment': 'right',
+                                           'verticalalignment': 'center'})
+    plt.tick_params(axis='y', labelsize=6)
+    plt.tick_params(axis='x', labelsize=8)
+    plt.grid(axis='x')
+    plt.gca().invert_yaxis()
+    plt.show()
 
-    #           4 ГРАФИК
+
+def fourth_plot(data):
     top_cities_vacancies = get_top_cities_vacancies(data)
     top_cities_vacancies['Другие'] = 1 - sum(top_cities_vacancies.values())
-    sub[1, 1].set_title(label="Доля вакансий по городам", fontsize=8)
-    sub[1, 1].pie(top_cities_vacancies.values(), labels=top_cities_vacancies.keys(),
-                  textprops={'fontsize': 6})
-    sub[1, 1].axis('equal')
-    fig.tight_layout()
-    fig.show()
-    return sub
+    plt.title(label="Доля вакансий по городам", fontsize=8)
+    plt.pie(top_cities_vacancies.values(), labels=top_cities_vacancies.keys(), textprops={'fontsize': 6})
+    plt.axis('equal')
+    plt.show()
 
 
-print(create_cur_data())
+def create_plot():
+    data = create_cur_data()
+    filtred_data = filter_data(data)
+
+    first_plot(data, filtred_data)
+    second_plot(data, filtred_data)
+    third_plot(data)
+    fourth_plot(data)
+
+    return -2
+
+
+if __name__ == '__main__':
+    create_plot()
---
+++ Analytics Scripts/creating_dataframe.py
@@ -0,0 +1,95 @@
+import xml.etree.ElementTree as ET
+import pandas as pd
+import requests
+import math
+import time
+
+
+def parse_all_id():
+    dictionary = {}
+    response = requests.get('https://www.cbr.ru/scripts/XML_valFull.asp')
+    root = ET.fromstring(response.text)
+
+    for record in root.findall('Item'):
+        id = record.get('ID')
+        char_code = record.find('ISO_Char_Code').text
+        dictionary[char_code] = id
+
+    return dictionary
+
+
+def create_dict_of_all_currency(dict_of_id):
+    result_dict = {}
+    for code in dict_of_id.values():
+        response = requests.get(f'https://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=24/01/2003&date_req2=01/01/2024&VAL_NM_RQ={code}')
+        if response.status_code != 200:
+            continue
+        dictionary = {}
+        root = ET.fromstring(response.text)
+        for record in root.findall('Record'):
+            date = '-'.join(record.get('Date').split('.')[::-1])
+            vunitrate = record.find('VunitRate').text
+            dictionary[date] = vunitrate
+
+        result_dict[code] = dictionary
+
+    return result_dict
+
+
+def calculate_salary(d, salary_from, salary_to, currency, published_at):
+    if pd.isna(salary_from) and pd.isna(salary_to):
+        return 0
+    if pd.isna(salary_from):
+        salary = math.floor(salary_to)
+    elif pd.isna(salary_to):
+        salary = math.floor(salary_from)
+    else:
+        salary = (salary_from + salary_to) / 2
+    if currency == 'RUR':
+        return salary
+
+
+    #TODO: ПЕРЕДЕЛАТЬ БЫ, ЧТО-БЫ ЕСЛИ НЕТУУ ДАТЫ, БРАЛАСЬ СЛЕДУЮЩАЯ
+    if published_at[:10] in d[currency]:
+        exchange_rate = float(d[currency][published_at[:10]].replace(',', '.'))
+    else:
+        return 0
+    salary = round(salary * exchange_rate, 2)
+    if salary <= 1_500_000:
+        return salary
+    else:
+        return 0
+
+
+def convert_currency_to_id(dictionary, x):
+    if x == 'BYR':
+        x = 'BYN'
+    if not pd.isna(x):
+        if x == 'RUR':
+            return 'RUR'
+        return dictionary[x]
+    else:
+        return ''
+
+
+def create_cur_data(csv='C:\\Users\\kiril\\PycharmProjects\\Web-App-on-Django\\files\\vacancies.csv'):
+    start = time.time()
+    data = pd.read_csv(csv)
+    dict_of_id = parse_all_id()
+    data['salary_currency'] = data['salary_currency'].apply(lambda x: convert_currency_to_id(dict_of_id, x))
+    dict_of_all_currency = create_dict_of_all_currency(dict_of_id)
+    data['salary'] = data.apply(lambda row: calculate_salary(dict_of_all_currency, row['salary_from'],
+                                                             row['salary_to'], row['salary_currency'],
+                                                             row['published_at']), axis=1)
+    data['published_at'] = data['published_at'].apply(lambda x: x[:10:])
+    data['published_at'] = pd.to_datetime(data['published_at'], format="%Y-%m-%d")
+    data['year'] = data['published_at'].dropna().dt.year
+    data.drop(['salary_from', 'salary_to', 'salary_currency', 'published_at'], axis=1, inplace=True)
+    end = time.time()
+    data.to_csv('C:\\Users\\kiril\\PycharmProjects\\Web-App-on-Django\\files\\cur_vac.csv')
+    print(end-start)
+    return data
+
+
+if __name__ == '__main__':
+    create_cur_data()
