4ead4dbbb9ff15ce524f3dca5a7803b5fc7547af
ManualCode <kiriltimoxov@gmail.com>
2024-01-20 20:47:04 +0500

    +static ALLOWED_HOSTS

--- djangoProject/main/views.py
+++ djangoProject/main/views.py
@@ -8,23 +8,23 @@ def index(request):
 
 
 def relevance(request):
-    df = pd.read_csv('main/static/deps/data_for_tables/first.csv', index_col=0)
+    df = pd.read_csv('/home/ManualCode/Web-App-on-Django/djangoProject/main/static/deps/data_for_tables/first.csv', index_col=0)
     return render(request, "main/relevance.html", {'frame': df})
 
 
 def geography(request):
-    df1 = pd.read_csv('main/static/deps/data_for_tables/second.csv', index_col=0)
-    df2 = pd.read_csv('main/static/deps/data_for_tables/second2.csv', index_col=0)
-    df3 = pd.read_csv('main/static/deps/data_for_tables/third.csv', index_col=0)
-    df4 = pd.read_csv('main/static/deps/data_for_tables/fourth.csv', index_col=0)
+    df1 = pd.read_csv('/home/ManualCode/Web-App-on-Django/djangoProject/main/static/deps/data_for_tables/second.csv', index_col=0)
+    df2 = pd.read_csv('/home/ManualCode/Web-App-on-Django/djangoProject/main/static/deps/data_for_tables/second2.csv', index_col=0)
+    df3 = pd.read_csv('/home/ManualCode/Web-App-on-Django/djangoProject/main/static/deps/data_for_tables/third.csv', index_col=0)
+    df4 = pd.read_csv('/home/ManualCode/Web-App-on-Django/djangoProject/main/static/deps/data_for_tables/fourth.csv', index_col=0)
 
     return render(request, "main/geography.html", {'frame1': df1, 'frame2': df2,
                                                    'frame3': df3, 'frame4': df4})
 
 
 def skills(request):
-    df = pd.read_csv('main/static/deps/data_for_tables/skills.csv', index_col=0)
-    fdf = pd.read_csv('main/static/deps/data_for_tables/filtred_skills.csv', index_col=0)
+    df = pd.read_csv('/home/ManualCode/Web-App-on-Django/djangoProject/main/static/deps/data_for_tables/skills.csv', index_col=0)
+    fdf = pd.read_csv('/home/ManualCode/Web-App-on-Django/djangoProject/main/static/deps/data_for_tables/filtred_skills.csv', index_col=0)
     years = [str(x) for x in range(2015, 2024)]
     return render(request, "main/skills.html", {'frame': df, 'filtred_frame': fdf, 'years': years})
 
